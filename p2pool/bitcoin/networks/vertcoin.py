2e0f33985dbe6c067a8d2b4f6980f0a856d0bd5d
Mark Zeits <mark@zeits.net>
2021-02-25 12:51:27 -0500

    Missed updating vertcoin.py in manual merge.

--- p2pool/bitcoin/networks/vertcoin.py
+++ p2pool/bitcoin/networks/vertcoin.py
@@ -27,21 +27,11 @@ POW_FUNC = lambda data: pack.IntType(256).unpack(verthash_hash(data))
 BLOCK_PERIOD = 150 # s
 SYMBOL='VTC'
 CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Vertcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Vertcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.vertcoin'), 'vertcoin.conf')
-<<<<<<< HEAD
-BLOCK_EXPLORER_URL_PREFIX='http://insight.vertcoin.org/block/'
-ADDRESS_EXPLORER_URL_PREFIX='http://insight.vertcoin.org/address/'
-TX_EXPLORER_URL_PREFIX='http://insight.vertcoin.org/tx/'
 SANE_TARGET_RANGE=(2**256//1000000000000000000 - 1, 2**256//100000 - 1)
-DUMB_SCRYPT_DIFF=16
-DUST_THRESHOLD=0.03e8
 HUMAN_READABLE_PART = 'vtc'
-=======
 BLOCK_EXPLORER_URL_PREFIX = 'https://chainz.cryptoid.info/vtc/block.dws?'
 ADDRESS_EXPLORER_URL_PREFIX = 'https://chainz.cryptoid.info/vtc/address.dws?'
 TX_EXPLORER_URL_PREFIX = 'https://chainz.cryptoid.info/vtc/tx.dws?'
-#SANE_TARGET_RANGE = (2**256//2**32//100000000 - 1, 2**256//2**32 - 1)
 SANE_TARGET_RANGE =  (2**256//100000000000000000 - 1,  2**256//100000 - 1)
 DUMB_SCRYPT_DIFF = 256
 DUST_THRESHOLD = 0.001e8
-#SANE_TARGET_RANGE = (2**256//1000000000000000000 - 1, 2**256//100000 - 1)
->>>>>>> 147a5d768ef25cc67bbd2fd0b8a516c65c9f8564
