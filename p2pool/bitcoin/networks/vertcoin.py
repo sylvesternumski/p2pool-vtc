b64afdf34f72ea3fade6d65c0912f83e551efa95
Mark Zeits <mark@zeits.net>
2021-04-25 09:23:18 -0400

    Updated setup-py2exe.py and reverted to loading verthash.dat to RAM

--- p2pool/bitcoin/networks/vertcoin.py
+++ p2pool/bitcoin/networks/vertcoin.py
@@ -9,9 +9,16 @@ from p2pool.util import pack
 
 import verthash
 
-def verthash_hash(dat):
-    return verthash.getPoWHash(dat)
+verthash_data = None
+if verthash_data is None:
+    with open('verthash.dat', 'rb') as f:
+        verthash_data = f.read()
 
+    verthash_sum = hashlib.sha256(verthash_data).hexdigest()
+    assert verthash_sum == 'a55531e843cd56b010114aaf6325b0d529ecf88f8ad47639b6ededafd721aa48'
+
+def verthash_hash(dat):
+    return verthash.getPoWHash(dat, verthash_data)
 
 P2P_PREFIX='fabfb5da'.decode('hex')
 P2P_PORT=5889
